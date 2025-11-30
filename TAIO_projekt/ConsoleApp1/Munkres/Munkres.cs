4df493bf810a308b8d9fa159a691f5b85eb027aa
Camille Ferros <steam.mikoja@gmail.com>
2025-11-30 23:52:48 +0100

    znalazlem cos jeszcze

--- TAIO_projekt/ConsoleApp1/Munkres/Munkres.cs
+++ TAIO_projekt/ConsoleApp1/Munkres/Munkres.cs
@@ -1,4 +1,7 @@
-﻿namespace SubgraphIsomorphism.Munkres;
+﻿using System;
+using System.Collections.Generic;
+
+namespace SubgraphIsomorphism.Munkres;
 
 internal class Munkres
 {
@@ -13,33 +16,35 @@ internal class Munkres
         var z0_row = -1;
         var z0_col = -1;
         var emin = int.MaxValue;
+
+        
         for (var i = 0; i < x; ++i)
         {
             var min = tab[i, 0];
             for (var j = 1; j < y; ++j)
-            {
                 if (tab[i, j] < min)
                     min = tab[i, j];
-            }
             for (var j = 0; j < y; ++j)
                 tab[i, j] -= min;
         }
+
+        
         for (var j = 0; j < y; ++j)
         {
             var min = tab[0, j];
             for (var i = 1; i < x; ++i)
-            {
                 if (tab[i, j] < min)
                     min = tab[i, j];
-            }
             for (var i = 0; i < x; ++i)
                 tab[i, j] -= min;
         }
+
+        
         for (var i = 0; i < x; ++i)
         {
             for (var j = 0; j < y; ++j)
             {
-                if (tab[i, j] == 0 && !row_covered[i] && !col_covered[j])
+                if (tab[i, j] == 0 && !row_covered[i] && !col_covered[j])       
                 {
                     star[i, j] = true;
                     row_covered[i] = true;
@@ -47,15 +52,18 @@ internal class Munkres
                 }
             }
         }
+
+       
         for (var i = 0; i < x; ++i)
             row_covered[i] = false;
-        Step1:
+
+    Step1:
+        
         for (var j = 0; j < y; ++j)
         {
+            col_covered[j] = false;
             for (var i = 0; i < x; ++i)
             {
-                if (col_covered[j])
-                    break;
                 if (star[i, j])
                 {
                     col_covered[j] = true;
@@ -63,36 +71,64 @@ internal class Munkres
                 }
             }
         }
-        for (var i = 0; i < y; ++i)
+
+       
+        for (var j = 0; j < y; ++j)
         {
-            if (!col_covered[i])
+            if (!col_covered[j])
                 goto Step2;
         }
         goto Break;
+
     Step2:
+        
+        emin = int.MaxValue;
+
         for (var i = 0; i < x; ++i)
         {
             for (var j = 0; j < y; ++j)
             {
-                if (tab[i, j] == 0 & !col_covered[j] & !row_covered[i])
+                
+                if (tab[i, j] == 0 && !col_covered[j] && !row_covered[i])
                 {
                     prime[i, j] = true;
-                    for (var k = 0; k < x; ++k)
+
+                   
+                    var starColInRow = -1;
+                    for (var c = 0; c < y; ++c)
                     {
-                        if (star[k, j])
+                        if (star[i, c])
                         {
-                            row_covered[k] = true;
-                            col_covered[j] = false;
-                            goto Step2;
+                            starColInRow = c;
+                            break;
                         }
                     }
-                    goto Step3;
+
+                    if (starColInRow == -1)
+                    {
+                        
+                        z0_row = i;
+                        z0_col = j;
+                        goto Step3;
+                    }
+                    else
+                    {
+                        
+                        row_covered[i] = true;
+                        col_covered[starColInRow] = false;
+                        
+                        goto Step2;
+                    }
                 }
-                if (tab[i, j] < emin & !col_covered[j] & !row_covered[i])
+
+               
+                if (!col_covered[j] && !row_covered[i] && tab[i, j] < emin)
                     emin = tab[i, j];
             }
         }
+
         goto Step4;
+
     Step3:
         var series = new List<(int r, int c)>();
         if (z0_row < 0 || z0_col < 0)
@@ -134,21 +170,24 @@ internal class Munkres
             series.Add((starRow, primedCol));
         }
 
+
         foreach (var (r, c) in series)
             star[r, c] = !star[r, c];
 
+
         for (var i = 0; i < x; ++i)
-        {
             for (var j = 0; j < y; ++j)
                 prime[i, j] = false;
-        }
 
 
         for (var i = 0; i < x; ++i) row_covered[i] = false;
         for (var j = 0; j < y; ++j) col_covered[j] = false;
 
+
         goto Step1;
+
     Step4:
+
         for (var i = 0; i < x; ++i)
         {
             for (var j = 0; j < y; ++j)
@@ -159,8 +198,11 @@ internal class Munkres
                     tab[i, j] -= emin;
             }
         }
+
+
         emin = int.MaxValue;
         goto Step2;
+
     Break:
         return (star, tab);
     }

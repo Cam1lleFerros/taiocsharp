0e218719ee73bddcbe88b36922c58ad723bdc295
Czarnecki Piotr 6 (STUD) <01180766@pw.edu.pl>
2025-11-30 21:08:40 +0100

    Program structure changes

--- TAIO_projekt/ConsoleApp1/Program.cs
+++ TAIO_projekt/ConsoleApp1/Program.cs
@@ -1,23 +1,53 @@
-﻿using TAiO;
+﻿using System.Security.Cryptography.X509Certificates;
+using TAiO;
 
 var (p, g) = Graph.ReadTwoFromFile("input.txt");
 
-var ullman = new ConsoleApp1.Ullman.Ullman(p, g);
+Graph g1, g2;
+if (p.size > g.size)
+{
+    g2 = p;
+    g1 = g;
+}
+else if (p.size == g.size)
+{
+    if (p.EdgeCount() >= g.EdgeCount())
+    {
+        g1 = p;
+        g2 = g;
+    }
+    else
+    {
+        g1 = g;
+        g2 = p;
+    }
+}
+else 
+{
+    g2 = g;
+    g1 = p;
+}
 
-var result = ullman.FindIsomorphism();
 
+bool exact = false; // Potrzebujemy jakoś to wczytywać na wejściu
 using var writer = File.CreateText("output.txt");
 
-writer.WriteLine("Mapping result:");
-
-if (result == null)
-    writer.WriteLine("No isomorphism found.");
+if (exact)
+{
+    var ullman = new Ullman(g1, g2);
+    var (result, matrix) = ullman.FindIsomorphism();
+    if (result)
+    {
+        writer.WriteLine("Graf mniejszy jest podgrafem większego. Dokładne mapowanie:");
+        // Jakoś wydrukuj tą macierz
+    }
+    else
+    {
+        // Wywował metodę extend z Ullmana
+        // Wydrukuj nowy graf
+    }
+}
 else
 {
-    for (var i = 0; i < result.GetLength(0); ++i)
-    {
-        for (var j = 0; j < result.GetLength(1); ++j)
-            writer.Write(result[i, j] ? "1 " : "0 ");
-        writer.WriteLine();
-    }
+    var 
 }

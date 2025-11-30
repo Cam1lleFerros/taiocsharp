fac29df70e8fb113d4286cc022da749d44384805
Czarnecki Piotr 6 (STUD) <01180766@pw.edu.pl>
2025-11-30 22:59:08 +0100

    Ullman Expand try 1

--- TAIO_projekt/ConsoleApp1/Ullman/Ullman.cs
+++ TAIO_projekt/ConsoleApp1/Ullman/Ullman.cs
@@ -1,51 +1,72 @@
-﻿namespace SubgraphIsomorphism.Ullman;
+﻿using SubgraphIsomorphism;
 
-public class Ullman(Graph p, Graph g)
+namespace TAiO;
+
+public class Ullman
 {
-    public Graph g = g, p = p;
+    public Graph g, p;
+
+    public Ullman(Graph p, Graph g)
+    {
+        this.g = g;
+        this.p = p;
+    }
 
     public int Rows => p.size;
     public int Cols => g.size;
-    private int depth_record = 0;
-    private bool[,]? matrix_record = null;
+    private int deepestMatch = 0;
+    private bool[,]? bestMatch = null;
 
     public (bool, bool[,]?) FindIsomorphism()
     {
         var mapping = new bool[Rows, Cols];
         var usedColumns = new bool[Cols];
         Array.Fill(usedColumns, false);
-        var found = false;
+
         for (var i = 0; i < Rows; ++i)
         {
             for (var j = 0; j < Cols; ++j)
             {
-                if (p.OutDegree(i) <= g.OutDegree(j))
-                {
+                if (p.OutDegree(i) <= g.OutDegree(j) && GetInDegree(p, i) <= GetInDegree(g, j))
                     mapping[i, j] = true;
-                    found = true;
-                }
             }
         }
-        if (!found)
-            return (false, null);    
+
+        for (var i = 0; i < Rows; ++i)
+        {
+            bool hasCandidate = false;
+            for (var j = 0; j < Cols; ++j)
+            {
+                if (mapping[i, j])
+                {
+                    hasCandidate = true;
+                    break;
+                }
+            }
+            if (!hasCandidate)
+                return (false, null);
+        }
+
         var res = Recurse(mapping, 0, usedColumns);
         if (res != null)
             return (true, res);
         else
-            return (false, matrix_record);
+            return (false, bestMatch);
     }
 
     public bool[,]? Recurse(bool[,] mapping, int currentRow, bool[] usedColumns, int depth = 0)
     {
-        if (depth > depth_record)
+        if (depth > deepestMatch)
         {
-            depth_record = depth;
-            matrix_record = (bool[,])mapping.Clone();
+            deepestMatch = depth;
+            bestMatch = (bool[,])mapping.Clone();
         }
+
         if (currentRow == Rows)
         {
             if (IsValidMapping(mapping))
                 return mapping;
+            return null;
         }
 
         var mprim = (bool[,])mapping.Clone();
@@ -53,15 +74,18 @@ public class Ullman(Graph p, Graph g)
 
         for (var col = 0; col < Cols; ++col)
         {
-            if (!usedColumns[col])
+            if (mprim[currentRow, col] && !usedColumns[col])
             {
                 usedColumns[col] = true;
+
+                var newMapping = (bool[,])mprim.Clone();
                 for (var i = 0; i < Cols; ++i)
-                    mprim[currentRow, i] = i == col;
-                var result = Recurse(mprim, currentRow + 1, usedColumns, depth + 1);
+                    newMapping[currentRow, i] = (i == col);
+
+                var result = Recurse(newMapping, currentRow + 1, usedColumns, depth + 1);
                 if (result != null)
                     return result;
-                mprim[currentRow, col] = false;
+
                 usedColumns[col] = false;
             }
         }
@@ -80,6 +104,7 @@ public class Ullman(Graph p, Graph g)
                 {
                     if (mprim[i, j])
                     {
+                        // Check outgoing edges
                         foreach (var np in p.OutNeighbours(i))
                         {
                             var hasMapping = false;
@@ -111,9 +136,41 @@ public class Ullman(Graph p, Graph g)
             var sum = 0;
             for (var j = 0; j < Cols; ++j)
                 sum += mapping[i, j] ? 1 : 0;
-            if (sum != 1) 
+            if (sum != 1)
                 return false;
         }
+
+        for (var i = 0; i < Rows; ++i)
+        {
+            for (var j = 0; j < Rows; ++j)
+            {
+                if (p.adjMatrix[i, j])
+                {
+                    var mappedI = -1;
+                    var mappedJ = -1;
+
+                    for (var k = 0; k < Cols; ++k)
+                    {
+                        if (mapping[i, k]) mappedI = k;
+                        if (mapping[j, k]) mappedJ = k;
+                    }
+
+                    if (mappedI != -1 && mappedJ != -1 && !g.adjMatrix[mappedI, mappedJ])
+                        return false;
+                }
+            }
+        }
         return true;
+    }
+
+    private static int GetInDegree(Graph graph, int node)
+    {
+        var degree = 0;
+        for (var i = 0; i < graph.size; ++i)
+        {
+            if (i != node && graph.adjMatrix[i, node])
+                ++degree;
+        }
+        return degree;
     }
 }

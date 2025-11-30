40c9d73084506ed07dd411ba32474301c501c5f5
Łukasz <01180780@pw.edu.pl>
2025-11-30 21:31:36 +0100

    Added command line argument parsing for exact/approximate option

--- TAIO_projekt/ConsoleApp1/Program.cs
+++ TAIO_projekt/ConsoleApp1/Program.cs
@@ -1,53 +1,72 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using TAiO;
 
-var (p, g) = Graph.ReadTwoFromFile("input.txt");
-
-Graph g1, g2;
-if (p.size > g.size)
+internal class Program
 {
-    g2 = p;
-    g1 = g;
-}
-else if (p.size == g.size)
-{
-    if (p.EdgeCount() >= g.EdgeCount())
+    private static void Main(string[] args)
     {
-        g1 = p;
-        g2 = g;
-    }
-    else
-    {
-        g1 = g;
-        g2 = p;
-    }
-}
-else 
-{
-    g2 = g;
-    g1 = p;
-}
+        var (p, g) = Graph.ReadTwoFromFile("input.txt");
+
+        Graph g1, g2;
+        if (p.size > g.size)
+        {
+            g2 = p;
+            g1 = g;
+        }
+        else if (p.size == g.size)
+        {
+            if (p.EdgeCount() >= g.EdgeCount())
+            {
+                g1 = p;
+                g2 = g;
+            }
+            else
+            {
+                g1 = g;
+                g2 = p;
+            }
+        }
+        else
+        {
+            g2 = g;
+            g1 = p;
+        }
+
+        bool exact = false; // Potrzebujemy jakoś to wczytywać na wejściu 
+                            // command line arguments i guess
+        if (args.Length == 1)
+        {
+            if(args[0] == "--exact")
+            {
+                exact = true;
+            }
+            else
+            {
+                exact = false;
+            }
+        }
 
 
-bool exact = false; // Potrzebujemy jakoś to wczytywać na wejściu
-using var writer = File.CreateText("output.txt");
+        using var writer = File.CreateText("output.txt");
 
-if (exact)
-{
-    var ullman = new Ullman(g1, g2);
-    var (result, matrix) = ullman.FindIsomorphism();
-    if (result)
-    {
-        writer.WriteLine("Graf mniejszy jest podgrafem większego. Dokładne mapowanie:");
-        // Jakoś wydrukuj tą macierz
+        if (exact)
+        {
+            var ullman = new Ullman(g1, g2);
+            var (result, matrix) = ullman.FindIsomorphism();
+            if (result)
+            {
+                writer.WriteLine("Graf mniejszy jest podgrafem większego. Dokładne mapowanie:");
+                // Jakoś wydrukuj tą macierz
+            }
+            else
+            {
+                // Wywował metodę extend z Ullmana
+                // Wydrukuj nowy graf
+            }
+        }
+        else
+        {
+            ;
+        }
     }
-    else
-    {
-        // Wywował metodę extend z Ullmana
-        // Wydrukuj nowy graf
-    }
-}
-else
-{
-    var 
 }

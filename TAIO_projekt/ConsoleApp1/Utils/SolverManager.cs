d7f6018ba1e859bbb34e2fd3f035056459abbc54
Czarnecki Piotr 6 (STUD) <01180766@pw.edu.pl>
2025-12-21 17:17:44 +0100

    Zmieniono instrukcję kompilacji

--- TAIO_projekt/ConsoleApp1/Utils/SolverManager.cs
+++ TAIO_projekt/ConsoleApp1/Utils/SolverManager.cs
@@ -27,7 +27,9 @@ public class SolverManager
 
     public void RegisterSolver(ISubgraphIsomorphismSolver solver) =>  solvers.Add(solver);
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     public SolverManager(string[] args)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     {
         SIOptions options = new(args);
 
